9a838d91bdeb20607b28bb6c852bee83cae4dc2d
tariktopcagic <tarik.topcagic@edu.fit.ba>
2024-08-04 21:36:18 +0200

    Added user update functionality and loading indicators

---
+++ client/src/app/_services/busy.service.ts
@@ -0,0 +1,26 @@
+import { inject, Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  busyRequestCount = 0;
+  private spinnerService = inject(NgxSpinnerService);
+
+  busy() {
+    this.busyRequestCount++;
+    this.spinnerService.show(undefined, {
+      type: 'ball-scale-ripple',bdColor: 'rgb(255,255,255,0)',
+      color: '#D8D8D8'
+    })
+  }
+
+  idle() {
+    this.busyRequestCount--;
+    if (this.busyRequestCount <= 0) {
+      this.busyRequestCount = 0;
+      this.spinnerService.hide();
+    }
+  }
+}
